a4452e65f6b2d841c372b2da9e390de6e88985bf
JUANSOLTEROBARRERA <72166031+JUANSOLTEROBARRERA@users.noreply.github.com>
2022-04-23 16:05:12 -0600

    Secciones

---
+++ resources/views/principal/pagprincipal.php
@@ -0,0 +1,11 @@
+@extends('layouts.plantilla')
+
+@section('Panelizq')
+    <a>ODA</a>
+@endsection
+@section('Titulo')
+ENTRADA DE INVENTARIO
+@endsection
+@section('AreaTrabajo')
+    <a>ODA</a>
+@endsection
