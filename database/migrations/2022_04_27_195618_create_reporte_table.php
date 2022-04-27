cdb9cbb6325773ec18f655fbd55788eab1805722
Sicon <jonathan3492@hotmail.com>
2022-04-27 14:05:08 -0600

    Migracion reportes

---
+++ database/migrations/2022_04_27_195618_create_reporte_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reporte', function (Blueprint $table) {
+            $table->id();
+            $table->string('nivel_educativo');
+            $table->string('institucion');
+            $table->string('carrera');
+            $table->string('tipo_violencia');
+            $table->string('quien_ejerce');
+            $table->string('sexo_agresor');
+            $table->string('lugar');
+            $table->string('accion_tomada');
+            $table->string('respuesta_accion');
+            $table->string('temporalidad');
+            $table->string('otro_servicio');
+            $table->longText('narracion');
+            $table->string('recibe_info');
+            $table->string('existen_mecanismos');
+            $table->boolean('resuelto');
+            $table->string('usuario');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reporte');
+    }
+};
