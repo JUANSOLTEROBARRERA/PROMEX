ea9caa803032dc3bf879a2a9a247e9c37ba58f80
JUANSOLTEROBARRERA <72166031+JUANSOLTEROBARRERA@users.noreply.github.com>
2022-04-28 08:07:47 -0600

    Arreglo de menÃú de Consultoría

--- resources/views/consultoria/consultoria.blade.php
+++ resources/views/consultoria/consultoria.blade.php
@@ -1,56 +1,55 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <link href="{{ asset('css/Estilo.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/Estilo3.css') }}" rel="stylesheet">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
-    
     </head>
     <body>
         <div class=".container-fluid">
         <header class="Encabezado">
+            <nav class="navbar navbar-expand-lg navbar-light">
+                <div class="container-fluid">
+                    <img class="logo" src="{{ asset('images/Logo.png')}}" onclick="window.location='{{url("/") }}'">
 
-
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <div class="container-fluid">
-                <img class="logo" src="{{ asset('images/Logo.png')}}" onclick="window.location='{{url("/") }}'">
-
-                        <div class="d-flex flex-row-reverse">
-                            <ul class="menu"  data-animation="bonus">
-                                <li><a class="oda" onclick="window.location='{{url("Test") }}'">Test de Violencia</a></li>
-                                <li><a class="oda" onclick="window.location='{{url("TiposViolencia") }}'">Tipos de Violencia</a></li>
-                                <li><a class="oda" onclick="window.location='{{url("Denuncia") }}'">Realiza tu Denuncia</a></li>
+                            <div class="d-flex flex-row-reverse">
+                                <ul class="menu"  data-animation="bonus">
+                                <li><a class="oda" onclick="window.location='{{url("Test") }}'">TEST DE VIOLENCIA</a></li>
+                                <li><a class="oda" onclick="window.location='{{url("TiposViolencia") }}'">TIPOS DE VIOLENCIA</a></li>
+                                <li><a class="oda" onclick="window.location='{{url("Denuncia") }}'">REALIZA TU DENUNCIA</a></li>
                                 @auth    
-                                <li><a class="oda" onclick="window.location='{{url("Consultoria") }}'">Consultoría</a></li>
+                                <li><a class="oda" onclick="window.location='{{url("Consultoria") }}'">CONSULTORÍA</a></li>
                                 <li><form action="/post-logout" method="POST">
                                     @csrf
-                                    <a class="oda" href="#" onclick="this.closest('form').submit()">Cerrar Sesión</a>
+                                    <a class="oda" href="#" onclick="this.closest('form').submit()">CERRAR SESIÓN</a>
                                 </form></li>
                                 @else
-                                <li><a class="oda" onclick="window.location='{{url("Login") }}'">Inicio de Sesión</a></li>
+                                <li><a class="oda" onclick="window.location='{{url("Login") }}'">INICIO DE SESIÓN</a></li>
                                 @endauth
-                            </ul>
-                        </div>
+                                </ul>
+                            </div>
 
-            </div>
+                </div>
             </nav>
-
-
-
         </header>
         </div>
-        <div class="container-fluid">
-                    <div id="Cuerpo">
-                        <div class="row justify-content-end">
 
 
-                            <a>Consulta</a>
 
+                                            <div id="Cuerpo2">
+                                            <div class="row justify-content-begin">
+                                                <div class="col-sm-6">
+                            
+                                                <a>La consultoría está en proceso.</a>
+                                    
+                                                </div>
+                                                </div>
+                                                </div>
 
-                        </div>
-                    </div>
-            </div>
+                            
+
+                            
     </body>
 </html>
