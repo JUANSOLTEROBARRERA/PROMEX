4c83453b27fe06df187180d619f3d169188d99d3
Sicon <jonathan3492@hotmail.com>
2022-05-15 10:50:05 -0600

    Tabla consultoria

--- resources/views/consultoria/consultoria.blade.php
+++ resources/views/consultoria/consultoria.blade.php
@@ -1,131 +1,129 @@
 <!DOCTYPE html>
 <html>
-    <head>
+
+<head>
         <link href="{{ asset('css/Estilo3.css') }}" rel="stylesheet">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
-    </head>
+</head>
 
 <body>
         <div class=".container-fluid">
-        <header class="Encabezado">
-            <nav class="navbar navbar-expand-lg navbar-light">
-                <div class="container-fluid">
-                    <img class="logo" src="{{ asset('images/Logo.png')}}" onclick="window.location='{{url("/") }}'">
+                <header class="Encabezado">
+                        <nav class="navbar navbar-expand-lg navbar-light">
+                                <div class="container-fluid">
+                                        <img class="logo" src="{{ asset('images/Logo.png')}}" onclick="window.location='{{url("/") }}'">
 
-                            <div class="d-flex flex-row-reverse">
-                                <ul class="menu"  data-animation="bonus">
-                                <li><a class="oda" onclick="window.location='{{url("TiposViolencia") }}'">TIPOS DE VIOLENCIA</a></li>
-                                <li><a class="oda" onclick="window.location='{{url("Test") }}'">TEST DE VIOLENCIA</a></li>
-                                <li><a class="oda" onclick="window.location='{{url("Denuncia") }}'">REALIZA TU DENUNCIA</a></li>
-                                @auth    
-                                <li><a class="oda" onclick="window.location='{{url("Consultoria") }}'">CONSULTORÍA</a></li>
-                                <li><form action="/post-logout" method="POST">
-                                    @csrf
-                                    <a class="oda" href="#" onclick="this.closest('form').submit()">CERRAR SESIÓN</a>
-                                </form></li>
-                                @else
-                                <li><a class="oda" onclick="window.location='{{url("Login") }}'">INICIO DE SESIÓN</a></li>
-                                @endauth
-                                </ul>
-                            </div>
-
-                </div>
-            </nav>
-
-        </header>
-        </div>
-<div>
-
-<div class="container mt-5">
-    <div class="col-12">
-        <div class="row">
-                <div class="col-12 grid-margin">
-
-                        <div style="text-align: right;"> <!-- botón -->
-                                <button type="button" class="btn btn-danger" style= "background-color: #00695c;color:#ffffffff; ">Genera reporte</button>                
-                        </div>
-
-                        <div class="card">
-                                <div class="card-body" style="background-color: rgb(47, 47, 47);">
-                                        <h4 class="card-title" style="color: rgb(74, 133, 34);">Incidente</h4>
-                                                <select class="form-field animation a3" name="institucion" id="institucion" placeholder="Institucion"style="color: rgb(74, 133, 34);">
-                                                                <option>- Institucion -</option>
-                                                                <option>ITTEPIC</option>
-                                                                <option>UAN</option>
-                                                </select> 
-                                  
-                                        <div class="table-responsive">
-                                                <table class="table">
-                                                        <thead>
-                                                                <tr style="background-color: #00695c; color:#FFFFFF;">
-                                                                        <th style=" text-align: center;"> Institucion </th>
-                                                                        <th style=" text-align: center;"> Sexo del agresor </th>
-                                                                        <th style=" text-align: center;"> Sexo del agredido </th>
-                                                                        <th style=" text-align: center;"> Fecha del incidente </th>
-                                                                </tr>
-
-                                                        </thead>                                                        
-                                                </table>
+                                        <div class="d-flex flex-row-reverse">
+                                                <ul class="menu" data-animation="bonus">
+                                                        <li><a class="oda" onclick="window.location='{{url("TiposViolencia") }}'">TIPOS DE VIOLENCIA</a></li>
+                                                        <li><a class="oda" onclick="window.location='{{url("Test") }}'">TEST DE VIOLENCIA</a></li>
+                                                        <li><a class="oda" onclick="window.location='{{url("Denuncia") }}'">REALIZA TU DENUNCIA</a></li>
+                                                        @auth
+                                                        <li><a class="oda" onclick="window.location='{{url("Consultoria") }}'">CONSULTORÍA</a></li>
+                                                        <li>
+                                                                <form action="/post-logout" method="POST">
+                                                                        @csrf
+                                                                        <a class="oda" href="#" onclick="this.closest('form').submit()">CERRAR SESIÓN</a>
+                                                                </form>
+                                                        </li>
+                                                        @else
+                                                        <li><a class="oda" onclick="window.location='{{url("Login") }}'">INICIO DE SESIÓN</a></li>
+                                                        @endauth
+                                                </ul>
                                         </div>
 
+                                </div>
+                        </nav>
 
+                </header>
+        </div>
+        <div>
+
+                <div class="container-fluid mt-5">
+                        <div class="col-13">
+                                <div class="row">
+                                        <div class="col-18 grid-margin">
+
+                                                <!-- <div style="text-align: right;">
+                                                        <button type="button" class="btn btn-danger" style="background-color: #00695c;color:#ffffffff; ">Genera reporte</button>
+                                                </div> -->
+
+                                                <div class="card">
+                                                        <div class="card-body" style="background-color: rgb(47, 47, 47);">
+                                                                <h4 class="card-title" style="color: rgb(74, 133, 34);">Tabla de denuncias</h4>
+                                                                
+                                                                <select class="form-field animation a3" name="sexo_agredido" id="sexo_agredido" placeholder="sexo_agredido">
+                                                                        <option>- Sexo agredido -</option>
+                                                                        <option>Hombre</option>
+                                                                        <option>Mujer</option>
+                                                                </select>
+                                                                <select class="form-field animation a3" name="sexo_agresor" id="sexo_agresor" placeholder="sexo_agresor">
+                                                                        <option>- Sexo agresor -</option>
+                                                                        <option>Hombre</option>
+                                                                        <option>Mujer</option>
+                                                                </select>
+                                                                <select class="form-field animation a3" name="tipo_violencia" id="tipo_violencia" placeholder="tipo_violencia">
+                                                                        <option>- Tipo violencia -</option>
+                                                                        <option>Verbal o psicológica</option>
+                                                                        <option>Patrimonial</option>
+                                                                        <option>Fisica</option>
+                                                                        <option>Sexual</option>
+                                                                </select>
+                                                                <select class="form-field animation a3" name="institucion" id="institucion" placeholder="institucion">
+                                                                        <option>- Institucion -</option>
+                                                                        <option>ITTEPIC</option>
+                                                                        <option>UAN</option>
+                                                                </select>
+                                                                <select class="form-field animation a3" name="lugar" id="lugar" placeholder="lugar">
+                                                                        <option>- Lugar -</option>
+                                                                        <option>Dentro del aula</option>
+                                                                        <option>En una oficina o privado</option>
+                                                                        <option>Áreas comunes</option>
+                                                                        <option>Baños</option>
+                                                                        <option>Fuera del plantel realizando una actividad académica</option>
+                                                                        <option>Fuera del plantel realizando otra actividad</option>
+                                                                </select>
+                                                                <br>
+                                                                <div style="margin-top: 10px;">
+                                                                <input type="button" class="btn btn-sm" value="Consultar" style="background-color: #00695c; color: white;">
+                                                                <input type="button" class="btn btn-sm" value="Generar reporte" style="background-color: #00695c; color: white;">
+                                                                </div>
+                                                                
+                                                                <br>
+
+                                                                <div class="table-responsive">
+                                                                        <table class="table">
+                                                                                <thead>
+                                                                                        <tr style="background-color: #00695c; color:#FFFFFF;">
+                                                                                                <th style=" text-align: center;"> Sexo agredido </th>
+                                                                                                <th style=" text-align: center;"> Nombre agresor </th>
+                                                                                                <th style=" text-align: center;"> Sexo agresor </th>
+                                                                                                <th style=" text-align: center;"> Tipo Violencia </th>
+                                                                                                <th style=" text-align: center;"> Institución </th>
+                                                                                                <th style=" text-align: center;"> Lugar </th>
+                                                                                                <th style=" text-align: center;"> Accion tomada </th>
+                                                                                                <th style=" text-align: center;"> Respuesta acción </th>
+                                                                                                <th style=" text-align: center;"> Temporalidad </th>
+                                                                                                <th style=" text-align: center;"> ¿Necesitó otro servicio? </th>
+                                                                                                <th style=" text-align: center;"> Detalles </th>
+                                                                                                <th style=" text-align: center;"> Correo </th>
+
+                                                                                        </tr>
+
+                                                                                </thead>
+                                                                        </table>
+                                                                </div>
+
+
+                                                        </div>
+                                                </div>
+                                        </div>
                                 </div>
                         </div>
                 </div>
-        </div>
+</body>
 
-    </div>
-</div>
-<!--tabla 2-->
-<div class="container mt-5">
-    <div class="col-12">
-        <div class="row">
-                <div class="col-12 grid-margin">
-                        <div class="card">
-                                       
-                                <div class="card-body"style="background-color: rgb(47, 47, 47);" >
-                                        <h4 class="card-title"style="color: rgb(74, 133, 34);">Institución</h4>
-
-                                                <select class="form-field animation a3" name="tipo" id="tipo" placeholder="tipo"style="color: rgb(74, 133, 34);">
-                                                <option>- Tipo -</option>
-                                                <option>Medio Superior</option>
-                                                <option>Superior</option>
-                                                </select> 
-                                                                                                                                        
-                                       <div class="table-responsive" >
-                                                <table class="table">
-                                                        <thead>
-                                                                <tr style="background-color: #00695c; color:#FFFFFF;">
-                                                                        <th style=" text-align: center;"> Institucion </th>
-                                                                        <th style=" text-align: center;"> Tipo </th>
-
-                                                                </tr>
-                                                        </thead>                                                        
-                                                </table>
-    
-                                        </div>
-                                </div>
-                        </div>
-                </div>
-        </div>
-
-    </div>
-</div>
-<!--tabla 2-->   
-</div> 
- <!-- 
-        <div class="container-fluid">
-                    <div id="Cuerpo">
-                        <div class="row justify-content-end">
-                            <a>Consulta</a>
-                        </div>
-                    </div>
-            </div>
-        -->   
-     </body>
- 
 </html>
- 
