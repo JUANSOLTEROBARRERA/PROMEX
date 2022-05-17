a868520b988be7181adb40562a75a68d65d12ecb
axlopezre <83840788+axlopezre@users.noreply.github.com>
2022-05-17 10:22:38 -0600

    Tipos de violencia avance

--- resources/views/tipos/tipos.blade.php
+++ resources/views/tipos/tipos.blade.php
@@ -3,10 +3,9 @@
 
 <head>
     <link href="{{ asset('css/tipos.css') }}" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 </head>
 
 <body>
@@ -40,113 +39,193 @@
         </header>
     </div>
 
-    <div id="Cuerpo2">
-        <div class="row justify-content-begin">
-            <div class="col-sm-9">
-
-
-                <div class="row justify-content-begin">
-                    <div class="col-sm-18">
-                        <section class="vh-100">
-                            <div class="container py-5 h-100">
-                                <div class="login-wrap">
-
-
-                                    <div class="card" style="width: 18rem;">
-
-
-                                        <img src="{{ asset('images/verbal.png')}}" style="padding:20px" alt="..." class="rounded">
 
 
 
-                                        <div class="card-body">
-                                            <h5 class="card-title">Violencia Verbal o Psicológica</h5>
-                                            <p class="card-text">La violencia verbal es cuando...</p>
-                                            <a href="#" class="btn btn-primary">Realiza tu denuncia</a>
+    <script lang="JavaScript">
+        $(document).ready(function() {
+
+            var navListItems = $('div.setup-panel div a'),
+                allWells = $('.setup-content'),
+                allNextBtn = $('.nextBtn');
+
+            allWells.hide();
+
+            navListItems.click(function(e) {
+                e.preventDefault();
+                var $target = $($(this).attr('href')),
+                    $item = $(this);
+
+                if (!$item.hasClass('disabled')) {
+                    navListItems.removeClass('btn-success').addClass('btn-default');
+                    $item.addClass('btn-success');
+                    allWells.hide();
+                    $target.show();
+                    $target.find('input:eq(0)').focus();
+                }
+            });
+
+            allNextBtn.click(function() {
+                var curStep = $(this).closest(".setup-content"),
+                    curStepBtn = curStep.attr("id"),
+                    nextStepWizard = $('div.setup-panel div a[href="#' + curStepBtn + '"]').parent().next().children("a"),
+                    curInputs = curStep.find("input[type='text'],input[type='url']"),
+                    isValid = true;
+
+                $(".form-group").removeClass("has-error");
+                for (var i = 0; i < curInputs.length; i++) {
+                    if (!curInputs[i].validity.valid) {
+                        isValid = false;
+                        $(curInputs[i]).closest(".form-group").addClass("has-error");
+                    }
+                }
+
+                if (isValid) nextStepWizard.removeAttr('disabled').trigger('click');
+            });
+
+            $('div.setup-panel div a.btn-success').trigger('click');
+        });
+    </script>
+
+    <!-- codigo nuevo-->
+
+    <!------ Include the above in your HEAD tag ---------->
+
+    <div class="container">
+        <div class="stepwizard">
+            <div class="stepwizard-row setup-panel">
+                <div class="stepwizard-step col-xs-3">
+                    <a href="#step-1" type="button" class="btn btn-success btn-circle">1</a>
+                    <p class="mr-5"><small>Verbal o Psicológica</small></p>
+                </div>
+                <div class="stepwizard-step col-xs-3">
+                    <a href="#step-2" type="button" class="btn btn-default btn-circle" disabled="disabled">2</a>
+                    <p><small>Patrimonial</small></p>
+                </div>
+                <div class="stepwizard-step col-xs-3">
+                    <a href="#step-3" type="button" class="btn btn-default btn-circle" disabled="disabled">3</a>
+                    <p><small>Física</small></p>
+                </div>
+                <div class="stepwizard-step col-xs-3">
+                    <a href="#step-4" type="button" class="btn btn-default btn-circle" disabled="disabled">4</a>
+                    <p><small>Sexual</small></p>
+                </div>
+                <div class="stepwizard-step col-xs-3">
+                    <a href="#step-4" type="button" class="btn btn-default btn-circle" disabled="disabled">4</a>
+                    <p><small>Personal Safety</small></p>
+                </div>
+            </div>
+        </div>
+
+        <form role="form">
+            <div class="panel panel-primary setup-content" id="step-1">
+                <div id="Cuerpo2">
+                    <div class="row justify-content-begin">
+                        <div class="col-sm-9">
+                            <div class="row justify-content-begin">
+                                <div class="col-sm-18">
+                                    <section class="vh-100">
+                                        <div class="container py-5 h-100">
+                                            <div class="login-wrap">
+                                                <div class="card" style="width: 18rem;">
+                                                    <img src="{{ asset('images/verbal.png')}}" style="padding:20px" alt="..." class="rounded">
+                                                    <div class="card-body">
+                                                        <h5 class="card-title">Violencia Verbal o Psicológica</h5>
+                                                        <p class="card-text">La violencia verbal es cuando...</p>
+                                                        <a href="#" class="btn btn-primary">Realiza tu denuncia</a>
+                                                    </div>
+                                                </div>
+                                            </div>
                                         </div>
-                                    </div>
-
                                 </div>
                             </div>
+                        </div>
                     </div>
                 </div>
             </div>
-
-
-        </div>
-    </div>
     </div>
 
-
-
-    <div class="container">
-
-	
-<div class="row mbr-justify-content-center">
-
-    <div class="col-lg-6 mbr-col-md-10">
-        <div class="wrap">
-            <div class="ico-wrap">
-                <span class="mbr-iconfont fa-volume-up fa"></span>
-            </div>
-            <div class="text-wrap vcenter">
-                <h2 class="mbr-fonts-style mbr-bold mbr-section-title3 display-5">Stay <span>Successful</span></h2>
-                <p class="mbr-fonts-style text1 mbr-text display-6">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum</p>
-            </div>
-        </div>
-    </div>
-    <div class="col-lg-6 mbr-col-md-10">
-        <div class="wrap">
-            <div class="ico-wrap">
-                <span class="mbr-iconfont fa-calendar fa"></span>
-            </div>
-            <div class="text-wrap vcenter">
-                <h2 class="mbr-fonts-style mbr-bold mbr-section-title3 display-5">Create
-                    <span>An Effective Team</span>
-                </h2>
-                <p class="mbr-fonts-style text1 mbr-text display-6">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum</p>
-            </div>
-        </div>
-    </div>
-    <div class="col-lg-6 mbr-col-md-10">
-        <div class="wrap">
-            <div class="ico-wrap">
-                <span class="mbr-iconfont fa-globe fa"></span>
-            </div>
-            <div class="text-wrap vcenter">
-                <h2 class="mbr-fonts-style mbr-bold mbr-section-title3 display-5">Launch
-                    <span>A Unique Project</span>
-                </h2>
-                <p class="mbr-fonts-style text1 mbr-text display-6">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum</p>
-            </div>
-        </div>
-    </div>
-    <div class="col-lg-6 mbr-col-md-10">
-        <div class="wrap">
-            <div class="ico-wrap">
-                <span class="mbr-iconfont fa-trophy fa"></span>
-            </div>
-            <div class="text-wrap vcenter">
-                <h2 class="mbr-fonts-style mbr-bold mbr-section-title3 display-5">Achieve <span>Your Targets</span></h2>
-                <p class="mbr-fonts-style text1 mbr-text display-6">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum</p>
-            </div>
-        </div>
+    <div class="panel panel-primary setup-content" id="step-2">
+    <div id="Cuerpo2">
+                    <div class="row justify-content-begin">
+                        <div class="col-sm-9">
+                            <div class="row justify-content-begin">
+                                <div class="col-sm-18">
+                                    <section class="vh-100">
+                                        <div class="container py-5 h-100">
+                                            <div class="login-wrap">
+                                                <div class="card" style="width: 18rem;">
+                                                    <img src="{{ asset('images/verbal.png')}}" style="padding:20px" alt="..." class="rounded">
+                                                    <div class="card-body">
+                                                        <h5 class="card-title">Patrimonial</h5>
+                                                        <p class="card-text">La violencia Patrimonial es cuando...</p>
+                                                        <a href="#" class="btn btn-primary">Realiza tu denuncia</a>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
     </div>
 
-    
+    <div class="panel panel-primary setup-content" id="step-3">
+    <div id="Cuerpo2">
+                    <div class="row justify-content-begin">
+                        <div class="col-sm-9">
+                            <div class="row justify-content-begin">
+                                <div class="col-sm-18">
+                                    <section class="vh-100">
+                                        <div class="container py-5 h-100">
+                                            <div class="login-wrap">
+                                                <div class="card" style="width: 18rem;">
+                                                    <img src="{{ asset('images/verbal.png')}}" style="padding:20px" alt="..." class="rounded">
+                                                    <div class="card-body">
+                                                        <h5 class="card-title">Física</h5>
+                                                        <p class="card-text">La violencia fisica es cuando...</p>
+                                                        <a href="#" class="btn btn-primary">Realiza tu denuncia</a>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+    </div>
 
-    
-</div>
+    <div class="panel panel-primary setup-content" id="step-4">
+    <div id="Cuerpo2">
+                    <div class="row justify-content-begin">
+                        <div class="col-sm-9">
+                            <div class="row justify-content-begin">
+                                <div class="col-sm-18">
+                                    <section class="vh-100">
+                                        <div class="container py-5 h-100">
+                                            <div class="login-wrap">
+                                                <div class="card" style="width: 18rem;">
+                                                    <img src="{{ asset('images/verbal.png')}}" style="padding:20px" alt="..." class="rounded">
+                                                    <div class="card-body">
+                                                        <h5 class="card-title">Sexual</h5>
+                                                        <p class="card-text">La violencia sexual es cuando...</p>
+                                                        <a href="#" class="btn btn-primary">Realiza tu denuncia</a>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+    </div>
+    </form>
+    </div>
 
-</div>
-
-
-
-
-
-
-
-
+    <!-- xd-->
 
 
 
